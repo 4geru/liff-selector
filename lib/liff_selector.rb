7cf79003b0146c3743fb651a3827514bd2614807
Rp7rf <westhouse51@gmail.com>
2018-06-17 10:21:29 +0900

    add clean function

--- lib/liff_selector.rb
+++ lib/liff_selector.rb
@@ -12,6 +12,8 @@ module LiffSelector
     when 'create'
       raise ArgumentError, 'give _type_ _url_' if argv.length != 3
       create(type: argv[1], url: argv[2])
+    when 'clean'
+      clean
     when 'delete'
       raise ArgumentError, 'give _liff_id_' if argv.length != 2
       delete(liff_id: argv[1])
@@ -59,8 +61,37 @@ module LiffSelector
     case response[:status]
     when 200
       puts '> [SUCESS] make application'
+      puts "> application uri : line://app/#{response[:liffId]}"
     else
       puts '> [FAILED] make application'
+    end
+  end
+
+  def self.clean
+    apps = get_all_application
+    uniq_app = apps.map{|app| {type: app['view']['type'], url: app['view']['url'] } }.uniq
+    delete_apps = uniq_app.map{|uapp|
+      same_apps = apps
+      .map.with_index{|app, i| app.store('id', i + 1); app }
+      .select{|app| uapp[:type] == app['view']['type'] and uapp[:url] == app['view']['url']}
+
+      puts ">== same application No.#{same_apps.map{|app| app['id'] }.join(' No.')} =="
+      puts "> \"type\" : #{uapp[:type]}\t \"url\" : #{uapp[:url]}"
+      same_apps[1..-1]
+    }.flatten!
+
+    unless delete_apps.empty?
+      begin
+        delete_apps.map{|app|
+          raise unless delete_delete_application(app['liffId'])
+          puts ">> delete \"id\" : #{app[:id]}\t\"type\" : #{app[:type]}\t\"url\" : #{app[:url]}"
+        }
+        puts '> [SUCESS] delete application'
+      rescue
+        puts '> [FAILED] cannot delete application'
+      end
+    else
+      puts ">> There is not same application"
     end
   end
 
